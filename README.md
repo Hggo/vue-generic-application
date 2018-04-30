d06421d0e19388b14691403a3c66bf978a1cc294
Hugo Rodrigues <hugo.ifce@gmail.com>
2018-04-30 15:38:54 -0300

    Erases unnecessary files and prepares for production

---
+++ README.md
@@ -0,0 +1,33 @@
+# vue-generic-application
+
+This project tries to generalize a lot of front-end functions, such as:
+
+- Consuming RESTful APIs
+- Creating routes
+- Building forms
+- Validating data
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'index.js',
+    library:'vue-generic-application',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+            "tsx": {
+              loader: "ts-loader",
+              options: {
+                appendTsxSuffixTo: [/\.vue$/],
+                compilerOptions: {
+                "jsx": "preserve"
+                }
+              }
+            }
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        }
+      }
+    ]
+  },
+  externals: {
+    vue: 'vue'
+  },
+  resolve: {
+    extensions: ['.ts', '.jsx','.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]
+}
